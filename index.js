bd1ab0b4ed886f259096dafab90792472b79621b
Anderson Santana <anderslash@gmail.com>
2025-07-23 16:32:00 -0300

    Added endpoint

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /*
 Objetivo: Vamos usar o serviço da viaCEP para buscar um endereço baseado no CEP.
+
+https://viacep.com.br/ws/01001000/json/
+
 - Crie uma função que receba como argumento o cep e retorne um objeto com logradouro, cidade e estado. 
  */
