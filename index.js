11e161ca178ccf2bfee0993c913941a184bb44a7
Anderson Santana <anderslash@gmail.com>
2025-07-23 15:26:20 -0300

    Remove error handling from buscarEnderecoPorCEP function for simplified response processing

--- index.js
+++ index.js
@@ -2,29 +2,17 @@ function buscarEnderecoPorCEP(cep) {
   
   return fetch(`https://viacep.com.br/ws/${cep}/json/`)
     .then(response => {
-      if (!response.ok) {
-        throw new Error(`Erro HTTP: ${response.status}`);
-      }
       return response.json();
     })
     .then(data => {
-      if (data.erro) {
-        throw new Error('CEP não encontrado');
-      }
-      
       return {
         logradouro: data.logradouro,
         cidade: data.localidade,
         estado: data.uf
       };
     })
-    .catch(error => {
-      console.error('Erro ao buscar CEP:', error);
-      throw error;
-    });
 }
 
 // Uso
 buscarEnderecoPorCEP('01001000')
-  .then(endereco => console.log(endereco))
-  .catch(erro => console.error(erro));
+  .then(endereco => console.log(endereco));
