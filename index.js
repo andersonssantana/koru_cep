aa1c606a95824fc7f184654b8b11c3f7ef283cb8
Anderson Santana <anderslash@gmail.com>
2025-07-23 15:09:01 -0300

    Add objective comment for viaCEP address lookup functionality

---
+++ index.js
@@ -0,0 +1,4 @@
+/*
+Objetivo: Vamos usar o serviço da viaCEP para buscar um endereço baseado no CEP.
+- Crie uma função que receba como argumento o cep e retorne um objeto com logradouro, cidade e estado. 
+ */
